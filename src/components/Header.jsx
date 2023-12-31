e9c7691ab47151263ed15f4fd999fb2cb0fca189
sanyam <patilsanyam869@gmail.com>
2023-12-31 12:28:27 +0530

    created navbar design

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+   
+      <NavContent/>
+    </nav>
+  )
+}
+
+const NavContent = () => {
+ return(
+
+  <>
+    <h2>sanyam</h2>
+    <div>
+      <a href='#home'>home</a>
+      <a href='#about'>about</a>
+      <a href='#services'>services</a>
+      <a href='#contact'>contact</a>
+    </div>
+    <a href='mailto:patilsanyam869@gmail.com'>
+      <button>Email</button>
+    </a>
+  </>
+  )
+}
+
+export default Header
