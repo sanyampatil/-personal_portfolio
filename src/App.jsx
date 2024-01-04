cafa3baa916cd3bebadcb0f812704b79ee7f7b50
sanyam <patilsanyam869@gmail.com>
2024-01-04 22:08:41 +0530

    start to build services page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
 import Skill from './components/Skill'
+import Services from './components/Services'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +13,7 @@ function App() {
     <Header/>
     <Home/>
     <Skill/>
+    <Services/>
 
     </>
 
